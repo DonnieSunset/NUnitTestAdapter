acee58de1aebc3ea24b6f3d3873b655a6dcd2a16
Randall Flagg <flagg1980@outlook.com>
2017-12-10 11:25:14 +0100

    initial commit

---
+++ MyLibraryTests/calculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using MyLibrary;
+using NUnit.Framework;
+using System;
+
+namespace MyLibraryTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator calc = null;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            calc = new Calculator();
+        }
+
+        [Test]
+        public void Add_twoPositiveNumbers_SumCorrect()
+        {
+            Console.WriteLine("SH: Adding 1 and 2");
+            Assert.That(calc.Add(1, 2), Is.EqualTo(3));
+            Console.WriteLine("SH: Success!");
+        }
+    }
+}
---
+++ MyNUnitTestAdapter/Adapter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnitTestAdapter
+{
+    [ExtensionUri(Id)]
+    public class VsTestExecutor : ITestExecutor
+    {
+        public const string Id = "executor://Fixie.VisualStudio";
+        public static Uri Uri = new Uri(Id);
+
+        public void Cancel()
+        {
+            Debugger.Launch();
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            Debugger.Launch();
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            Debugger.Launch();
+        }
+    }
+
+    [DefaultExecutorUri(VsTestExecutor.Id)]
+    [FileExtension(".dll")]
+    public class VsTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            discoverySink.SendTestCase(new TestCase("Testcase1", VsTestExecutor.Uri, "mySource"));
+            discoverySink.SendTestCase(new TestCase("Testcase2", VsTestExecutor.Uri, "mySource"));
+            discoverySink.SendTestCase(new TestCase("Testcase3", VsTestExecutor.Uri, "mySource"));
+            Debugger.Launch();
+
+        }
+    }
+}
